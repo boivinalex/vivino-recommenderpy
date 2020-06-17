06517da18b5a99a0f78dadc1829e4d243cab716d
Alex Boivin <30531248+boivinalex@users.noreply.github.com>
2020-06-17 15:32:33 -0400

    Get reviews

--- scrape.py
+++ scrape.py
@@ -19,6 +19,24 @@ import re
 
 URL = 'https://www.vivino.com/explore?e=eJwNyUEKgCAQBdDb_LVC21lE3SIiJptESI1RrG6fm7d5UckihkTWIPJLg4H7aBrhOjPuvv6kxhqk8oW8k3INyZeNmyh7QaZDisNTl5XsD-oNGk4='
 
+class element_present_after_scrolling():
+    """
+    An expectation for checking that an element is present.
+    
+    locator - used to find the element
+    returns the WebElement once it has the particular element
+    """
+    def __init__(self, locator, driver):
+        self.locator = locator
+        self.driver = driver
+    def __call__(self, driver):
+        # self.driver._debug("element_present_after_scrolling::Finding Elements")
+        elements = driver.find_elements(*self.locator)   # Finding the referenced element
+        if len(elements) > 0:
+            return elements
+        else:
+            self.driver.execute_script("window.scrollBy(0, 500);")
+
 class wine_data():
     """Scrape wine data and reviews from Vivino."""
     
@@ -59,7 +77,7 @@ class wine_data():
         if self.scroll_to_bottom:
             self._infinity_scroll()
                 
-        self.wine_data = self._get_wine_info()
+        self.wine_data, self.review_data = self._get_wine_info()
                 
     def _infinity_scroll(self,element=False):
         """Infinite scroll to bottom of page."""
@@ -67,7 +85,7 @@ class wine_data():
             el = element
         else:
             el = self.driver.find_element_by_class_name('inner-page')
-        SCROLL_PAUSE_TIME = 0.5 # wait a bit before each scroll
+        SCROLL_PAUSE_TIME = 1 # wait a bit before each scroll
         # Get scroll height
         last_height = self.driver.execute_script("return arguments[0].scrollHeight", el)
         while True:
@@ -98,16 +116,35 @@ class wine_data():
         """
         global discover_wines
         discover_wines = self.driver.find_elements_by_class_name('vintageTitle__winery--2YoIr')
-        global dict_list
-        dict_list = []
+        global wine_dict_list
+        global review_dict_list
+        wine_dict_list = []
+        review_dict_list = []
         
         ##TEST
-        del discover_wines[0]
+        # del discover_wines[0]
+        discover_wines = discover_wines[1:3]
         
         # for i, wine in enumerate(discover_wines):
         for wine in discover_wines:
+            # open wine page in new tab
             wine.click()
-            self.driver.switch_to.window(self.driver.window_handles[1]) #switch to latest tab (firefox always opens a new tab next to the main tab)
+            # switch to latest tab (firefox always opens a new tab next to the main tab)
+            self.driver.switch_to.window(self.driver.window_handles[1]) 
+            
+            # make top of page is loaded
+            timeout = 20
+            global element_present
+            try: 
+                element_present = EC.presence_of_element_located((By.CLASS_NAME, 'inner-page'))
+                WebDriverWait(self.driver, timeout).until(element_present)
+            except TimeoutException:
+                print("Timed out waiting for tab to load")
+            
+            # if show more reviews button is below the loaded page, scroll until it loads
+            element_present = WebDriverWait(self.driver, timeout).until(element_present_after_scrolling((By.CLASS_NAME, 'anchor__anchor--3DOSm.communityReviews__showAllReviews--1e12c.anchor__vivinoLink--29E1-'), self.driver))
+            
+            #make sure all relevant page items have loaded
             timeout = 20
             try: #make sure all relevant page items have loaded
                 element_present = EC.presence_of_element_located((By.CLASS_NAME, 'anchor__anchor--3DOSm.communityReviews__showAllReviews--1e12c.anchor__vivinoLink--29E1-'))
@@ -123,8 +160,8 @@ class wine_data():
             wine_rating_number = self.driver.find_element_by_class_name('vivinoRatingWide__basedOn--s6y0t').text
             wine_price = self.driver.find_element_by_class_name('purchaseAvailabilityPPC__amount--2_4GT').text
             global wine_dict
-            wine_dict = {'Name':wine_name,'Winery':winery_name,'Country':wine_country,'Rating':wine_rating,'NumberOfRatings':wine_rating_number,'Price':wine_price}
-            dict_list.append(wine_dict)
+            wine_dict = {'WineName':wine_name,'Winery':winery_name,'Country':wine_country,'Rating':wine_rating,'NumberOfRatings':wine_rating_number,'Price':wine_price}
+            wine_dict_list.append(wine_dict)
             
             # get reviews
             review_link = self.driver.find_element_by_class_name('anchor__anchor--3DOSm.communityReviews__showAllReviews--1e12c.anchor__vivinoLink--29E1-')
@@ -136,14 +173,25 @@ class wine_data():
                 print("Timed out waiting for tab to load")
             review_pane = self.driver.find_element_by_class_name('baseModal__window--3r5PC.baseModal__themeNoPadding--T_ROG')
             self._infinity_scroll(element=review_pane)
+            # get review info
+            global discover_reviews
+            discover_reviews = self.driver.find_elements_by_class_name('reviewCard__reviewContainer--1kMJM')
+            for review in discover_reviews:
+                user_name = review.find_element_by_class_name('anchor__anchor--3DOSm.reviewCard__userName--2KnRl').text
+                rating_elem = review.find_element_by_class_name('rating__rating--ZZb_x')
+                rating = float(rating_elem.get_attribute("aria-label").split()[1])
+                global review_dict
+                review_dict = {'Username':user_name,'WineName':wine_name,'Rating':rating}
+                review_dict_list.append(review_dict)
             
             ##TEST
-            break
+            # break
         
             self.driver.switch_to.window(self._main_window)
             time.sleep(1) # pause for 1 second 
-        wine_data = pd.DataFrame(dict_list)
+        wine_data = pd.DataFrame(wine_dict_list)
+        review_data = pd.DataFrame(review_dict_list)
             
-        return wine_data
+        return wine_data, review_data
         
         
