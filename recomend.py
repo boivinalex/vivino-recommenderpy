0b63a48b39dc7126f71def3da07d5e11b0d748e5
Alex Boivin <30531248+boivinalex@users.noreply.github.com>
2020-06-27 13:21:39 -0400

    Add docstrings. Add comments. Clean up.

--- recomend.py
+++ recomend.py
@@ -6,7 +6,6 @@ Created on Thu Jun 18 15:52:37 2020.
 """
 
 import pandas as pd
-import numpy as np
 import random
 from surprise import SVD
 from surprise import Dataset
@@ -24,19 +23,43 @@ class processed_wine_data():
     
     def __init__(self,wine_data,min_number_of_reviews=10):
         """
-        Filter data from wine_data instance.
+        Clean data from wine_data instance.
 
         Parameters
         ----------
-        wine_data : TYPE
-            DESCRIPTION.
-        min_number_of_reviews : TYPE, optional
-            DESCRIPTION. The default is 3.
+        wine_data : Instance of wine_data class.
+            Scraped wine reviews from Vivino.
+        min_number_of_reviews : int, optional
+            Minimum number of wines each user must have reviewed to be in the 
+            data set. The default is 10.
 
-        Returns
-        -------
-        None.
-
+        Attributes
+        ----------
+        num_scraped_reviews : int
+            Total number of scraped reviews.
+        num_cleaned_reviews : int
+            Total number of reviews left after cleaning.
+        num_wines : int
+            Total number of wines in cleaned data set.
+        num_unique_users : int
+            Total number of unique users in cleaned data set.
+        num_users_with_multiple_interactions : int
+            Number of users who reviewed at least min_number_of_reviews wines.
+        num_ratings_from_filtered_users : int
+            Number of reviews from users who reviewed at least 
+            min_number_of_reviews wines.
+        cleaned_review_data : DataFrame
+            Cleaned data set containing all users. Columns: Username, WineName,
+            Winery, Rating.
+        ratings_from_filtered_users_data : DataFrame
+            Cleaned data set containing only users who reviewed at 
+            least min_number_of_reviews wines. Columns: Username, WineName,
+            Winery, Rating.
+        combined_ratings_from_filtered_users_data : DataFrame
+            Cleaned data set containing only users who reviewed at 
+            least min_number_of_reviews wines with WineName and Winery columns
+            combined. Columns: Username, Wine, Rating.
+            
         """
         # Parameters
         self.wine_data = wine_data
@@ -63,26 +86,6 @@ class processed_wine_data():
         avaeraging reviews for users who rated the same wine multiple
         times, and removing anonymous reviews. Then only keep reviews from
         users with at least min_number_of_reviews.
-
-        Returns
-        -------
-        total_scraped_reviews : TYPE
-            DESCRIPTION.
-        TYPE
-            DESCRIPTION.
-        wine_count : TYPE
-            DESCRIPTION.
-        TYPE
-            DESCRIPTION.
-        TYPE
-            DESCRIPTION.
-        TYPE
-            DESCRIPTION.
-        cleaned_review_data_df : TYPE
-            DESCRIPTION.
-        ratings_from_filtered_users_df : TYPE
-            DESCRIPTION.
-
         """
         total_scraped_reviews = len(self.wine_data.review_data)
         print('Total scraped reviews: {}'.format(total_scraped_reviews))
@@ -111,7 +114,6 @@ class processed_wine_data():
         combined_ratings_from_filtered_users_df = combined_ratings_from_filtered_users_df[['Username','Wine','Rating']]
         
         
-        
         return total_scraped_reviews, len(cleaned_review_data_df), wine_count,\
             len(user_reviews_count_df), len(users_with_multiple_interactions_df),\
             len(ratings_from_filtered_users_df), cleaned_review_data_df,\
@@ -121,32 +123,86 @@ class processed_wine_data():
 class wine_recomender():
     """Vivino collaborative filtering recomender system."""
     
-    def __init__(self,processed_wine_data,n_splits=5,k_predictions=5,tune=False):
+    def __init__(self,processed_wine_data,n_splits=5,k_predictions=5,\
+                 tune=False,tune_method='rmse'):
+        """
+        Single Value Decomposition-based wine recomender system for Vivino.
         
+        Recomend new wines to users based on reviews posted on Vivino using SVD.
+        Fit is cross-validated using KFold cross-validation. Model 
+        hyperparameters can be automatically tuned using the Surprise package.
+        Tuning aims to improve fit but may yeild worse results (check!). Tuning
+        can aim to improve any of the accuracy metrics used by Surprise. After
+        (optionally) tuning and cross-validation, fit is perfromed on all data
+        to produce recomendations for all users in the data set.
 
+        Parameters
+        ----------
+        processed_wine_data : Instance of processed_wine_data
+            Cleaned data set with colmuns Username, Wine, Rating, in that order.
+        n_splits : int, optional
+            Number of splits or folds to perform in cross-validation step. 
+            The default is 5.
+        k_predictions : int, optional
+            Number of wine recomendations to make. The default is 5.
+        tune : bool, optional
+            If True, automatically tune hyperparameters. The default is False.
+        tune_method : str, optional
+            If tune is True, sets the accuracy measure which should be optimized
+            while tuning. Can be any of the Surprise accuracy metrics. 
+            The default is 'rmse'.
+
+        Attributes
+        ----------
+        top_k_items : defaultdict
+            Top k_predictions wines recomended to each user in the data set in
+            the form of a defaultdict where the key coresponds to the users.
+        top_k_items_pd : DataFrame
+            Top k_predictions wines recomended to each user in the data set in
+            the form of a pandas DataFrame with columns: Username, Wine, est. 
+            Where est is the predicted rating of that wine for that user.
+        all_predictions : list of surprise.prediction_algorithms.predictions.Prediction objects
+            All predictions for all users and all wines in the form of  
+            Surprise Prediction objects.
+
+        """
+        # Data
         self.data = processed_wine_data.combined_ratings_from_filtered_users_data
         self.data_ml = Dataset.load_from_df(self.data, reader=Reader(rating_scale=(1,5)))
         
+        # Parameters
         self.n_splits = n_splits
         self.k_predictions = k_predictions
         self.tune = tune
+        self.tune_method = tune_method
         
+        # Attributes
         self.top_k_items, self.top_k_items_pd, self.all_predictions = self.make_predictions()
         
         
     def make_predictions(self):
-        # Cross validation
+        """
+        Predict ratings of un-rated wines based on past ratings and SVD.
+
+        Returns
+        -------
+        top_k_items : defaultdict
+            Top k recomended wines.
+        top_k_items_pd : DataFrame
+            Top k recomended wines. Columns: Username, Wine, est.
+        predictions : list of surprise.prediction_algorithms.predictions.Prediction objects
+            All rating predictions for all users and all wines.
+
+        """
+        # Tuning
         # if tune, always compare tuned and un-tuned cross-validation results
         if self.tune:
-            # tuner = hyper_tune(self.data_ml)
-            # tuned_algo = tuner()
             tuned_algo = self.hyper_tune()
-            # cross-validate with 4 folds corresponding to a 75/25 split
-            # cross_validate(tuned_algo, self.data_ml, measures=['RMSE', 'MAE'], cv=4, verbose=True)
         algo = SVD()
-        # cross_validate(algo, self.data_ml, measures=['RMSE', 'MAE'], cv=4, verbose=True)
         
-        # cross-validate with 5 folds corresponding to a 80/20 split
+        # Cross-validation
+        # cross-validate with n_splits folds. 
+        # 5 folds (default) corresponds to a 80/20 split
         kf = KFold(n_splits=self.n_splits)
         # initialize cross-validation measures
         measures = ['rmse','mae','preci.@k','recall@k']
@@ -164,39 +220,45 @@ class wine_recomender():
         train_time = []
         test_time = []
         
+        # perform cross-validation
         for trainset, testset in kf.split(self.data_ml):
             # train and test algorithm
             if self.tune:
                 start_time = time.time()
-                tuned_algo.fit(trainset)
+                tuned_algo.fit(trainset) # train
                 train_time_tuned.append(time.time() - start_time)
                 start_time = time.time()
-                tuned_predictions = tuned_algo.test(testset)
+                tuned_predictions = tuned_algo.test(testset) # test
                 test_time_tuned.append(time.time() - start_time)
             start_time = time.time()
-            algo.fit(trainset)
+            algo.fit(trainset) # train
             train_time.append(time.time() - start_time)
             start_time = time.time()
-            # global predictions
-            predictions = algo.test(testset)
+            predictions = algo.test(testset) # test
             test_time.append(time.time() - start_time)
             
             # compute metrics
             if self.tune:
+                # get rmse and mae
                 rmse_tuned_vals.append(accuracy.rmse(tuned_predictions,verbose=False))
                 mae_tuned_vals.append(accuracy.mae(tuned_predictions,verbose=False))
-                tuned_precisions, tuned_recalls = self.precision_recall_at_k(tuned_predictions, k=self.k_predictions, threshold=3.5)
-                # Precision and recall can then be averaged over all users
+                # get precision@k and recall@k
+                tuned_precisions, tuned_recalls = self.precision_recall_at_k(tuned_predictions,threshold=3.5)
+                # average precision@k and recall@k over all users
                 precision_tuned_vals.append(sum(prec for prec in tuned_precisions.values()) / len(tuned_precisions))
                 recall_tuned_vals.append(sum(rec for rec in tuned_recalls.values()) / len(tuned_recalls))
+            # get rmse and mae
             rmse_vals.append(accuracy.rmse(predictions,verbose=False))
             mae_vals.append(accuracy.mae(predictions,verbose=False))
-            precisions, recalls = self.precision_recall_at_k(predictions, k=self.k_predictions, threshold=3.5)
-            # Precision and recall can then be averaged over all users
+            # get precision@k and recall@k
+            precisions, recalls = self.precision_recall_at_k(predictions,threshold=3.5)
+            # average precision@k and recall@k over all users
             precision_vals.append(sum(prec for prec in precisions.values()) / len(precisions))
             recall_vals.append(sum(rec for rec in recalls.values()) / len(recalls))
             
         # print metrics
+        # take advantage of surprise.model_selection.validation.print_summary
+        # test metrics results must be in the form of a dict of lists
         if self.tune:
             test_measures_tuned_dict = {}
             test_measures_tuned_list = [rmse_tuned_vals,mae_tuned_vals,precision_tuned_vals,recall_tuned_vals]
@@ -207,6 +269,7 @@ class wine_recomender():
                 test_measures_tuned_dict[m] = test_measures_tuned_list[i]
             test_measures_dict[m] = test_measures_list[i]
         
+        # use surprise.model_selection.validation.print_summary to print summary of results
         if self.tune:
             print('Tuned Cross-Validation Results:')
             surprise.model_selection.validation.print_summary(tuned_algo,measures,test_measures_tuned_dict,None,train_time_tuned,test_time_tuned,self.n_splits)
@@ -215,38 +278,56 @@ class wine_recomender():
             
         # Make recomendations
         # only recomend using tuned OR un-tuned algorithm
+        # train on the full data set
         full_trainset = self.data_ml.build_full_trainset()
         if self.tune:
             start_time = time.time()
-            tuned_algo.fit(full_trainset)
+            tuned_algo.fit(full_trainset) # train
             train_time = time.time() - start_time
             print("Took {} seconds for tuned full training.".format(train_time))
         else:
             start_time = time.time()
-            algo.fit(full_trainset)
+            algo.fit(full_trainset) # train
             train_time = time.time() - start_time
             print("Took {} seconds for un-tuned full training.".format(train_time))
         
-        # all user-item pairs with no rating in the trainset
+        # all user-item pairs with no rating in the trainset (don't recomend already rated wines)
         anti_testset = trainset.build_anti_testset()
         if self.tune:
             start_time = time.time()
-            predictions = tuned_algo.test(anti_testset)
+            predictions = tuned_algo.test(anti_testset) # predict
             test_time = time.time() - start_time
             print("Took {} seconds for tuned predictions.".format(test_time))    
         else:
             start_time = time.time()
-            predictions = algo.test(anti_testset)
+            predictions = algo.test(anti_testset) # predict
             test_time = time.time() - start_time
             print("Took {} seconds for un-tuned predictions.".format(test_time))    
             
         # Get top-k predictions for all users
-        top_k_items, top_k_items_pd = self.get_top_k(predictions, k=self.k_predictions)
+        top_k_items, top_k_items_pd = self.get_top_k(predictions)
         
         return top_k_items, top_k_items_pd, predictions
         
 
-    def hyper_tune(self,tune_method='rmse'):
+    def hyper_tune(self):
+        """
+        Use Surprises RandomizedSearchCV to tune SVD model hyperparameters.
+        
+        As recomended by https://surprise.readthedocs.io/en/stable/FAQ.html ,
+        split the data set into an A and B set to allow for unbiased accuracy
+        evaluation of the tuned parameters.
+        
+        RandomizedSearchCV is much faster than GridSearchCV when data set is
+        not small.
+
+        Returns
+        -------
+        algo : Tuned Surprise algorithm object
+            Can be used to train and test.
+
+        """
+        tune_method = self.tune_method
         print('Tuning...')
         # Seperate data into A and B sets for unbiased accuracy evaluation
         raw_ratings = self.data_ml.raw_ratings
@@ -269,7 +350,7 @@ class wine_recomender():
         print("Took {} seconds for search.".format(search_time))
         # best score
         print('Best score: ' + str(gs.best_score[tune_method]))
-        # combination of parameters that gave the best RMSE score
+        # combination of parameters that gave the best score according to the tune_method
         print('Best params: ' + str(gs.best_params[tune_method]))
         
         # get resulting algorithm with tuned parameters
@@ -295,12 +376,35 @@ class wine_recomender():
         accuracy.mae(predictions)
         accuracy.fcp(predictions)
         
-        return(algo)
+        return algo
         
         
-    def precision_recall_at_k(self,predictions, k=5, threshold=3.5):
-        """Return precision and recall at k metrics for each user."""
-    
+    def precision_recall_at_k(self,predictions, threshold=3.5):
+        """
+        Calculate precision and recall at k.
+        
+        Code from https://surprise.readthedocs.io/en/stable/FAQ.html
+        
+        Number of predictions set by k_predictions.
+
+        Parameters
+        ----------
+        predictions : surprise.prediction_algorithms.predictions.Prediction object
+            Predictions on the test set.
+        threshold : float, optional
+            Lowest rating to use for metrics. The default is 3.5.
+
+        Returns
+        -------
+        precisions : dict
+            Precision@k results.
+        recalls : dict
+            Recall@k results.
+
+        """
+        # Value of k
+        k = self.k_predictions
+        
         # First map the predictions to each user.
         user_est_true = defaultdict(list)
         for uid, _, true_r, est, _ in predictions:
@@ -309,43 +413,44 @@ class wine_recomender():
         precisions = dict()
         recalls = dict()
         for uid, user_ratings in user_est_true.items():
-    
-            # Sort user ratings by estimated value
+            # sort user ratings by estimated value
             user_ratings.sort(key=lambda x: x[0], reverse=True)
-    
-            # Number of relevant items
+            # number of relevant items
             n_rel = sum((true_r >= threshold) for (_, true_r) in user_ratings)
-    
-            # Number of recommended items in top k
+            # number of recommended items in top k
             n_rec_k = sum((est >= threshold) for (est, _) in user_ratings[:k])
-    
-            # Number of relevant and recommended items in top k
+            # number of relevant and recommended items in top k
             n_rel_and_rec_k = sum(((true_r >= threshold) and (est >= threshold))
                                   for (est, true_r) in user_ratings[:k])
-    
-            # Precision@K: Proportion of recommended items that are relevant
+            # precision@K: Proportion of recommended items that are relevant
             precisions[uid] = n_rel_and_rec_k / n_rec_k if n_rec_k != 0 else 1
-    
-            # Recall@K: Proportion of relevant items that are recommended
+            # recall@K: Proportion of relevant items that are recommended
             recalls[uid] = n_rel_and_rec_k / n_rel if n_rel != 0 else 1
     
         return precisions, recalls
 
 
-    def get_top_k(self,predictions, k=5):
-        """Return the top-N recommendation for each user from a set of predictions.
-    
-        Args:
-            predictions(list of Prediction objects): The list of predictions, as
-                returned by the test method of an algorithm.
-            n(int): The number of recommendation to output for each user. Default
-                is 10.
-    
-        Returns:
-        A dict where keys are user (raw) ids and values are lists of tuples:
-            [(raw item id, rating estimation), ...] of size n.
+    def get_top_k(self,predictions):
         """
-    
+        Return the top-K recommendation for each user from a set of predictions.
+        
+        From https://surprise.readthedocs.io/en/stable/FAQ.html
+
+        Parameters
+        ----------
+        predictions : surprise.prediction_algorithms.predictions.Prediction object
+            Predictions on all users.
+
+        Returns
+        -------
+        top_k : defaultdict
+            Top k recomended wines. Keys are users.
+        top_k_pd : DataFrame
+            Top k recomended wines. Columns: Username, Wine, est.
+
+        """
+        # Value of k
+        k = self.k_predictions
         # First map the predictions to each user.
         top_k = defaultdict(list)
         for uid, iid, true_r, est, _ in predictions:
